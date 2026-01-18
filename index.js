8be77805a90f2445cf9937455ead2d161aa61134
ibrahimvau5518 <jibonm676@gmail.com>
2026-01-18 16:22:04 +0600

    added mock database

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+// Mock Database
+let items = [
+  {
+    id: 1,
+    name: 'Laptop',
+    price: 1200,
+    description: 'Powerful gaming laptop',
+    image: 'https://picsum.photos/seed/1/200',
+  },
+  {
+    id: 2,
+    name: 'Phone',
+    price: 800,
+    description: 'Latest smartphone',
+    image: 'https://picsum.photos/seed/2/200',
+  },
+];
 
 app.listen(5000, () => console.log('Server running on http://localhost:5000'));
